5d2d27d5935d7379225d1b1884d981a93f20053f
Akst Elena <akstelena@yandex.ru>
2023-02-22 14:33:10 +0300

    add Example 008 Dog

---
+++ Example008_Dog/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача с собакой, когда друзья движутся навстречу друг другу
+
+int count = 0;
+double distance = 100;
+double FirstFriendSpeed = 1;
+double SecondFriendSpeed = 2;
+double DogSpeed = 3;
+int friend = 2;
+double time = 0;
+
+while(distance > 1)
+{
+  if(friend == 2)
+  {
+    time = distance / (SecondFriendSpeed + DogSpeed);
+    friend = 1;
+  }
+  time = distance / (FirstFriendSpeed + DogSpeed);
+  friend = 2;
+  
+  distance = distance - (SecondFriendSpeed + FirstFriendSpeed) * time;
+
+  count = count + 1;
+}
+
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");
