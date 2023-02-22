e00e2fd0d6d0679c4cc4f44402b62d3129eb1dad
Akst Elena <akstelena@yandex.ru>
2023-02-22 11:29:40 +0300

    add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿int numberA = 3; //для переменных не принято использовать нижнее подчеркивание, но можно цифры, главное с них не начинать
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
