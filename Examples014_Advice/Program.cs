0a3e6b4a2d3cda5be0a24006e738a4a771d7ed95
Akst Elena <akstelena@yandex.ru>
2023-03-15 11:51:11 +0300

    Examles_014

---
+++ Examples014_Advice/Program.cs
@@ -0,0 +1,18 @@
+﻿// Узнать тип данных
+
+// int a = 12;
+// Console.WriteLine(a.GetType());
+
+// int d = 12;
+// Console.WriteLine(d.GetType().Name);
+
+using System.Linq;
+
+// var b = 12;
+// Console.WriteLine(b.GetType().Name);
+
+var data = new int[] { 1, 2, 3, 4 }
+          .Where(e => e > 0)
+          .Select(e => new { q = e, w = e + 1 });
+
+Console.WriteLine(data.GetType().Name);
