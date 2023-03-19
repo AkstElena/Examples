e3e66b23be62aca5feff75f95c5b9b9ccbd20564
Akst Elena <akstelena@yandex.ru>
2023-03-19 15:56:58 +0300

    Лекции 5 и 6

--- Examples014_Advice/Program.cs
+++ Examples014_Advice/Program.cs
@@ -11,8 +11,26 @@ using System.Linq;
 // var b = 12;
 // Console.WriteLine(b.GetType().Name);
 
-var data = new int[] { 1, 2, 3, 4 }
-          .Where(e => e > 0)
-          .Select(e => new { q = e, w = e + 1 });
+// var data = new int[] { 1, 2, 3, 4 }
+//           .Where(e => e > 0)
+//           .Select(e => new { q = e, w = e + 1 });
 
-Console.WriteLine(data.GetType().Name);
+// Console.WriteLine(data.GetType().Name);
+
+
+void DrawText(string text, int left, int top)
+{
+  Console.SetCursorPosition(left, top);
+  Console.WriteLine(text);
+}
+//Так нельзя из-за магических чисел!!!
+//DrawText("Intensive C# Demo text", 629, 360)
+
+string caption = "Intensive C# Demo text";
+int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2; // Определить начало текста по середине экрана по ширине
+int screenHeighPosition = Console.WindowHeight / 2; // Определить начало текста по середине экрана по высоте
+DrawText( // При таком оформлении удобнее делать потом замену
+  text: caption,
+  left: screenWidthPosition,
+  top: screenHeighPosition
+  );
